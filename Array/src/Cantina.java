f40d1539ed921aea0694763f4b2bc08179977c95
Henriquefcastro <152014023+hfc10@users.noreply.github.com>
2024-09-02 23:44:03 -0300

    Add files via upload

---
+++ Array/src/Cantina.java
@@ -0,0 +1,26 @@
+public class Cantina {
+
+    String nome;
+
+    Salgado[] salgados = new Salgado[10];
+
+    void addSalgado(Salgado novoSalgado) {
+
+        for (int i = 0; i < salgados.length; i++) {
+            if (salgados[i] == null) {
+                salgados[i] = novoSalgado;
+                break;
+            }
+        }
+    }
+
+    void mostraInfo() {
+
+        for (int i = 0; i < salgados.length; i++) {
+
+            if (salgados[i] != null) {
+                System.out.println(salgados[i].nome);
+            }
+        }
+    }
+}
